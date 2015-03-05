12ff1995f11a61d56451e26392454af35d080e08
Kevin Wurster <wursterk@gmail.com>
2015-03-05 13:41:45 -0500

    Fixed a bug causing operations to be performed out of order.  Added documentation.

--- streaming-topology-operations.py
+++ streaming-topology-operations.py
@@ -3,7 +3,7 @@
 
 """
 Stream geometry from stdin perform one or more topology operations and write
-to stdout.
+to stdout.  See main() docstring for more info.
 """
 
 
@@ -13,10 +13,6 @@ import sys
 import click
 import shapely.geometry
 from str2type import str2type
-try:
-    from fiona.transform import transform_geom
-except ImportError:
-    transform_geom = None
 
 
 @click.command()
@@ -32,19 +28,38 @@ def main(topology_operation, skip_failures):
 
     """
     Perform topology operations on GeoJSON features or geometries.
+
+    By no means perfect or complete but multiple operations can be chained
+    together with the `-to` flag.
+
+    Examples:
+
+        Buffer geometries 15 meters:
+        $ fio cat sample-data/polygon-samples.geojson | \
+            ./streaming-topology-operations.py \
+            -to buffer:distance=15
+
+        Compute geometry centroid and buffer that by 100 meters:
+        $ fio cat sample-data/polygon-samples.geojson | \
+            ./streaming-topology-operations.py \
+            -to centroid -to buffer:distance=100
+
+        Compute geometry centroid, buffer 20 meters, and compute envelope:
+        $ fio cat sample-data/polygon-samples.geojson | \
+            ./streaming-topology-operations.py \
+            -to centroid -to buffer:distance=20 -to envelope
+
+    To dump to a file pipe to `fio load`.
     """
 
     # Parse topology operations into a more usable format
-    topo_ops = {}
+    topo_ops = []
     for definition in topology_operation:
         name = definition.split(':')[0]
         if len(definition.split(':')) > 1:
-            topo_ops[name] = {a.split('=')[0]: str2type(a.split('=')[1]) for a in definition.split(':')[1:]}
+            topo_ops.append((name, {a.split('=')[0]: str2type(a.split('=')[1]) for a in definition.split(':')[1:]}))
         else:
-            topo_ops[name] = {}
-    if transform_geom is None and 'transform_geom' in topo_ops:
-        click.echo("ERROR: `transform_geom' topology operation specified but could not import Fiona.", err=True)
-        sys.exit(1)
+            topo_ops.append((name, {}))
 
     for idx, item in enumerate(sys.stdin):
         try:
@@ -59,25 +74,25 @@ def main(topology_operation, skip_failures):
                 geom = shapely.geometry.asShape(item)
 
             # Perform all topology operations in order
-            for name, args in topo_ops.items():
-                if name == 'transform_geom':
-                    args.update(geom=shapely.geometry.mapping(geom))
-                    geom = shapely.geometry.asShape(transform_geom(**args))
-                else:
+            for name, args in topo_ops:
 
-                    # Some operations don't take any arguments so to prevent triggering argument errors don't even
-                    # try giving arguments to an operation if the user didn't specify any.  Let the user be responsible
-                    # for researching the arguments
-                    if not args:
-                        geom = getattr(geom, name)()
-                    else:
-                        geom = getattr(geom, name)(**args)
+                # Some operations don't take any arguments or are properties so to prevent triggering argument
+                # errors don't even try giving arguments to an operation if the user didn't specify any.  Let
+                # the user be responsible for researching the arguments.
+                operation = getattr(geom, name)
+                if not hasattr(operation, '__call__'):
+                    geom = operation  # Operation was a property so we already have the value via getattr()
+                elif not args:
+                    geom = operation()
+                else:
+                    geom = operation(**args)
 
             # Print to stdout
             if feature is not None:
                 feature['geometry'] = shapely.geometry.mapping(geom)
                 click.echo(json.dumps(feature))
             else:
+                pass
                 click.echo(json.dumps(shapely.geometry.mapping(geom)))
 
         except Exception as e:
